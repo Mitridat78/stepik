584ed5564e19d977909458c8fabe54262e1e7a37
unknown <mitridat78@gmail.com>
2022-06-20 10:42:16 +0300

    new lessons

--- python_django/numbers1.py
+++ python_django/numbers1.py
@@ -3,15 +3,37 @@
 за пределы 32-битного целочисленного диапазона со знаком [-231, 231-1], 
 то верните 0.Ограничения: -231 <= x <= 231-1
 """
-old_num = int(input())
+"""old_num = int(input())
 new_num = 0
 
 while old_num % 10 > 0:
 #    if old_num % 10 > 0:
     new_num += old_num % 10
-    new_num *= 10
     old_num = old_num // 10
+    new_num *= 10
+    
 if (-2)**31 <= new_num <= (2**31) - 1:
     print(new_num)
 else:
     print(0)
+    """
+
+
+old_num = input()
+
+if int(old_num) > 0:
+    new_num = old_num[::-1]
+    if (-2)**31 <= int(new_num) <= (2**31) - 1:
+        print(int(new_num))
+    else:
+        print(0)
+elif int(old_num) == 0:
+    print(0)
+else:
+    new_num = 0
+    new_num = old_num[len(old_num)-1:0:-1]
+    new_num=int(new_num)*(-1)
+    if (-2)**31 <= int(new_num) <= (2**31) - 1:
+        print(int(new_num))
+    else:
+        print(0)
