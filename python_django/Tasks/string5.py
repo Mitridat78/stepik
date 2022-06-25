937f7a2bcf3658aa904877158ab492a863f0ab0e
unknown <mitridat78@gmail.com>
2022-06-25 13:42:13 +0300

    project blog

---
+++ python_django/Tasks/string5.py
@@ -0,0 +1,10 @@
+"""
+Дан массив с символами. Разверни его в обратную сторону.
+Sample Input:
+h,e,l,l,o
+
+Sample Output:
+['o', 'l', 'l', 'e', 'h']
+"""
+word = input().split(',')
+print(word[::-1])
