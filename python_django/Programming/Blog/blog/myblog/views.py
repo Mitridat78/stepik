c10e75386511659db2d989d6e67d40f2683ffac1
unknown <mitridat78@gmail.com>
2022-06-29 00:32:12 +0300

    project blog

--- python_django/Programming/Blog/blog/myblog/views.py
+++ python_django/Programming/Blog/blog/myblog/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
 from django.views import View
+from .models import Post
+
 
 class MainView(View):
     def get(self, request, *args, **kwargs):
-        return render(
-            request,
-            'myblog/index.html'
-        )
-
+        posts = Post.objects.all()
+        return render(request, 'myblog/index.html', context={
+            'posts': posts
+        })
 # Create your views here.
